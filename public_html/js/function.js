ae2f3f2d717527ce93d70374e55ebbdb67807958
Naoki Sugihara <sugihara.blossom@gmail.com>
2015-02-14 03:30:06 +0900

    Modyfied AddForm.

--- public_html/js/function.js
+++ public_html/js/function.js
@@ -9,12 +9,15 @@ $(function(){
 		console.log(d_id);
 
 	});
+
+	$('p').data('role', 'add_btn').on('click', function(){
+		var tb = $(this).closest('thead').next('tbody');
+		var d_id = tb.children('tr:last').find('td').data('id');
+		var class_name = tb.children('tr:last').find('td').attr('class');		
+console.log(class_name);
+		tb.append('<tr><td class='+class_name+' data-id='+(d_id+1)+'>test</td><input type="hidden" name="title_num[]" class="title_num[]" value=0><td class="title_num[]", data-id='+(d_id+1)+'>0</td></tr>');
+	});
+
 });
 
 
-
-
-
-function test(){
-	console.log('test');
-}
