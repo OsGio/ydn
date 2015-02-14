7da09321a667211801e132566e628ed814781e68
Naoki Sugihara <sugihara.blossom@gmail.com>
2015-02-14 11:14:00 +0900

    Modyfied JS.

--- public_html/js/function.js
+++ public_html/js/function.js
@@ -9,7 +9,7 @@ $(function(){
 		console.log(d_id);
 
 	});
-
+/*
 	$('p').data('role', 'add_btn').on('click', function(){
 		var tb = $(this).closest('thead').next('tbody');
 		var d_id = tb.children('tr:last').find('td').data('id');
@@ -17,7 +17,19 @@ $(function(){
 console.log(class_name);
 		tb.append('<tr><td class='+class_name+' data-id='+(d_id+1)+'>test</td><input type="hidden" name="title_num[]" class="title_num[]" value=0><td class="title_num[]", data-id='+(d_id+1)+'>0</td></tr>');
 	});
+*/
 
+	$('p').data('role', 'add_btn').on('click', function(){
+		var tb = $(this).closest('thead').next('tbody');
+		var d_id = tb.children('tr:last').data('id');
+		var class_name = tb.children('tr').find('td').attr('class');
+
+		console.log(d_id);
+//		console.log(class_name);
+		
+		tb.children('tr:last').clone().appendTo(tb);
+		console.log(d_id);
+	});
 });
 
 
