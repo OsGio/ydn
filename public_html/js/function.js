c2edb1772ac5294112c5984159047e353b8f337e
Naoki Sugihara <sugihara.blossom@gmail.com>
2015-02-15 23:09:31 +0900

    Modyfied. (alert js bug in atom)

--- public_html/js/function.js
+++ public_html/js/function.js
@@ -1,34 +1,29 @@
 $(function(){
-	$('td[class^="ad_ads_title"] > :text').on('keyup', function(){
+
+	//function setNum()
+	$('td[class="ad_ads_title"] > input:text').on('keypress', function(){
 		var num = $(this).val().length;
-		var d_id = $('td[class^="ad_ads_title"]').data('id');
-		$('td[class^="title_num"]').text(num);
-		$('input[class^="title_num"]').val(num);
-
-
-		console.log(d_id);
-
+		$(this).closest('td').next().text(num);
+		$(this).closest('tr').find('input:hidden').val(num);
 	});
-/*
+
+	//function addForm()
 	$('p').data('role', 'add_btn').on('click', function(){
 		var tb = $(this).closest('thead').next('tbody');
-		var d_id = tb.children('tr:last').find('td').data('id');
-		var class_name = tb.children('tr:last').find('td').attr('class');		
-console.log(class_name);
-		tb.append('<tr><td class='+class_name+' data-id='+(d_id+1)+'>test</td><input type="hidden" name="title_num[]" class="title_num[]" value=0><td class="title_num[]", data-id='+(d_id+1)+'>0</td></tr>');
-	});
-*/
-
-	$('p').data('role', 'add_btn').on('click', function(){
-		var tb = $(this).closest('thead').next('tbody');
-		var d_id = tb.children('tr:last').data('id');
+		var d_id = tb.children().filter('tr:last').data('id');
 		var class_name = tb.children('tr').find('td').attr('class');
 
 		console.log(d_id);
-//		console.log(class_name);
 		
-		tb.children('tr:last').clone().appendTo(tb);
-		console.log(d_id);
+		var newtr = tb.children().filter('tr:last').clone(true);
+//console.log(newtr.data('id'));		
+		newtr.data('id', (d_id+1));
+		newtr.appendTo(tb);
+		//newtr.data('id', (d_id+1));
+		//console.log(newtr);
+		//newtr.clone(true).appendTo(tb);	
+//console.log(newtr.data('id'));
+
 	});
 });
 
